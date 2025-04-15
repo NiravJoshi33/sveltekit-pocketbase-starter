253c817ef5c9573476ceed8939ab8f6c66225ba7
Nirav Joshi <niravjoshi3000@gmail.com>
2025-04-15 07:24:07 +0530

    added css, layout and navbar

---
+++ src/lib/components/navbar.svelte
@@ -0,0 +1,36 @@
+<nav>
+	<ul>
+		<li><a href="/">Home</a></li>
+		<li><a href="/auth/login">Login</a></li>
+		<li><a href="/auth/register">Register</a></li>
+	</ul>
+</nav>
+
+<style>
+	nav {
+		display: flex;
+		flex-direction: row;
+		justify-content: center;
+		align-items: center;
+	}
+
+	ul {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-around;
+		align-items: center;
+		list-style-type: none;
+		gap: 4em;
+		padding: 0.25em 0;
+	}
+
+	a {
+		font-size: 1.2rem;
+		color: var(--text-secondary);
+		text-decoration: none;
+	}
+
+	a:hover {
+		color: var(--dark-celadon);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<section class="hero">
+	<h1>Sveltekit Pocketbase Starter</h1>
+	<p>Minimal Starter Template to Get Started With Sveltekit & Pocketbase</p>
+</section>
+
+<style>
+	.hero {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		height: 100vh;
+	}
+
+	.hero h1 {
+		font-size: 4rem;
+		font-weight: 800;
+		margin-bottom: 0.5em;
+	}
+
+	.hero p {
+		font-size: 1.5rem;
+		margin-top: 0.5em;
+		color: var(--text-secondary);
+	}
+</style>
